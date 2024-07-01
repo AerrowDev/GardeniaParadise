bfbef53f203bb484d5889f8e26d5aee25ec4a79a
Aaron <sgaeprellegera@gmail.com>
2024-07-01 21:20:29 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css"; 
+import "./globals.css";
 import { layoutMetadata } from "@/metadata";
 import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Promise<Metadata> = layoutMetadata();
+export const metadata = layoutMetadata();
 
 export default function RootLayout({
   children,
